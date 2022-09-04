78ada76426a505b817893cda8466c0f1e91b2fcd
Alan Ayala <kanonbolt128@gmail.com>
2022-09-04 16:27:32 -0500

    Genisis

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,31 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/AlanBayWalker0',
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/hireAlanAyala',
+    icon: 'github',
+  },
+];
---
+++ todo.md
@@ -0,0 +1,4 @@
+switch contact api to hit my firebase in functions/index.js
+switch .env
+reference read me
+deploy on aws
